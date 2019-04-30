9553a1c20d7a2dc7a58001f34bd7b1148f3f93d3
Majid Ataee <ataiemajid_63@hotmail.com>
2019-04-30 14:37:14 +0430

    Build date picker

--- dist/date-picker.js
+++ dist/date-picker.js
@@ -372,7 +372,7 @@ const DatePicker = (($) => {
      */
 
     const NAME = 'datePicker';
-    const VERSION = '1.3.5';
+    const VERSION = '1.3.6';
     const DATA_KEY = 'bs.date-picker';
     const EVENT_KEY = `.${DATA_KEY}`;
     const DATA_API_KEY = '.data-api';
