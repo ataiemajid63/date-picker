80305dfd9d18d15a7efee7cf8304356d904d12b3
Majid Ataee <ataiemajid_63@hotmail.com>
2019-09-23 11:10:09 +0330

    Update packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp');
+const { src, dest, parallel, watch } = require('gulp');
 const concat = require('gulp-concat');
 const minify = require('gulp-minify');
 const rename = require('gulp-rename');
@@ -7,40 +7,38 @@ const replace = require('gulp-replace');
  
 const regex = /^(import|export).*;{1}$/gm;
 
-gulp.task('js-build', function(){
-  return gulp.src(['./node_modules/pasoon-calendar-view/src/*.js', './src/*.js'], {base:'./'})
+function jsBuild(){
+  return src(['./node_modules/pasoon-calendar-view/src/*.js', './src/*.js'], {base:'./'})
     .pipe(concat('date-picker.js'))
     .pipe(replace(regex, ''))
-    .pipe(gulp.dest('dist'))
-});
+    .pipe(dest('dist'))
+};
 
-gulp.task('js-minify', function(){
-  return gulp.src(['./node_modules/pasoon-calendar-view/src/*.js', './src/*.js'], {base:'./'})
+function jsMinify(){
+  return src(['./node_modules/pasoon-calendar-view/src/*.js', './src/*.js'], {base:'./'})
     .pipe(concat('date-picker.js'))
     .pipe(replace(regex, ''))
     .pipe(minify())
-    .pipe(gulp.dest('dist'))
+    .pipe(dest('dist'))
     .pipe(rename('calendar-view.min.js'))
-});
+};
 
-gulp.task('sass-build', function () {
-  return gulp.src('./src/*.scss')
+function sassBuild() {
+  return src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
-});
+    .pipe(dest('./dist'));
+};
 
-gulp.task('sass-minify', function () {
-  return gulp.src('./src/*.scss')
+function sassMinify() {
+  return src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
-});
+    .pipe(dest('./dist'));
+};
 
-gulp.task('default', ['sass-minify', 'js-minify']);
-
-gulp.task('minify', ['sass-minify', 'js-minify']);
-
-gulp.task('build', ['sass-build', 'js-build']);
+exports.default = parallel(sassMinify, jsMinify);
+exports.minify = parallel(sassMinify, jsMinify);
+exports.build = parallel(sassBuild, jsBuild);
  
-gulp.task('watch', function () {
-  gulp.watch('./src/**/*.*', ['sass-build','js-build']);
-});
+exports.watch = function () {
+  watch('./src/**/*.*', parallel(sassBuild, jsBuild));
+};
